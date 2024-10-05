46eede3b7b485c0d04047811be7b556117adccea
Emre Yilmaz <emreyilmaz7c6@gmail.com>
2024-10-05 11:55:59 +0000

    Fix mobile button layout

--- src/app/ShareButton.tsx
+++ src/app/ShareButton.tsx
@@ -3,7 +3,7 @@
 export function ShareButton() {
   return (
     <button
-      className="cursor-pointer rounded-md bg-slate-950 py-2.5 px-3.5 text-sm font-semibold text-white shadow-sm hover:bg-slate-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+      className="cursor-pointer whitespace-nowrap rounded-md bg-slate-950 py-2.5 px-3.5 text-sm font-semibold text-white shadow-sm hover:bg-slate-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
       type="button"
       onClick={async () => {
         const shareData = {
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { ShareButton } from '@/app/ShareButton';
 export default function Home() {
   return (
     <main className="flex min-h-screen items-center justify-center p-4">
-      <article className="prose prose-slate rounded-md bg-slate-50 p-4 shadow-md">
+      <article className="prose prose-slate rounded-md bg-white p-4 shadow-md">
         <p className="md:prose-xl">
           This 👈👉 is money snek. 🐍🐍💰💰 Share this ⬆️⬆️🔜🔜 in 7.123
           7️⃣1️⃣2️⃣3️⃣ snekonds 🐍🐍 or you ✋✋ will NEVER ❌❌❌❌ get monies 💰💰
@@ -11,17 +11,19 @@ export default function Home() {
           time!!🕛🕧🕐🕜🕑🕝🕝 You 😏😏 may never ❌❌get monies 💰💰🐍💰💰
           again!!
         </p>
-        <div className="flex justify-end gap-x-4">
+        <div className="flex flex-wrap items-center gap-x-4 gap-y-8">
+          <div className="flex w-full md:w-auto">
+            <a
+              className="ml-auto whitespace-nowrap"
+              href="https://photoai.com/?via=moneysnek"
+              target="blank"
+              rel="noreferrer"
+            >
+              Use Photo AI 📸📸
+            </a>
+          </div>
           <a
-            className="mr-auto"
-            href="https://photoai.com/?via=moneysnek"
-            target="blank"
-            rel="noreferrer"
-          >
-            Checkout Photo AI 📸
-          </a>
-          <a
-            className="cursor-pointer rounded-md bg-white py-2.5 px-3.5 text-sm font-semibold text-gray-900 ring-1 shadow-sm ring-gray-300 ring-inset hover:bg-gray-50"
+            className="not-prose ml-auto cursor-pointer whitespace-nowrap rounded-md bg-white py-2.5 px-3.5 text-sm font-semibold text-gray-900 ring-1 shadow-sm ring-gray-300 ring-inset hover:bg-gray-50"
             href="https://x.com/emreloperr/status/1842169726245474377"
             target="_blank"
             rel="noreferrer"
